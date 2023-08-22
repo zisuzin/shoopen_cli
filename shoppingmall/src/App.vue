cd4e04bcb03671b3ca9d03b6f2f2b740e1534925
zisuzin <zi_soo_zin@naver.com>
2023-08-23 06:21:59 +0900

    카카오map api 호출

---
+++ shoppingmall/src/App.vue
@@ -0,0 +1,14 @@
+<!-- <template>
+  <Kakao-map></Kakao-map>
+</template>
+
+<script>
+  export default {
+    components: {
+      KakaoMap,
+    },  
+  }
+</script>
+
+<style>
+</style> -->
---
+++ shoppingmall/src/main.js
@@ -0,0 +1,10 @@
+// import Vue from 'vue'; 
+// import KakaoMap from "../src/components/Kakaomap.vue";
+
+// // 카카오맵 컴포넌트
+// Vue.component('Map-api', KakaoMap);
+
+// // root 인스턴스 생성
+// new Vue({
+//     el: '#app'
+// });
