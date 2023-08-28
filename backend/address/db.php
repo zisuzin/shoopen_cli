794fdb1a1ee5e633773957ecdca53226181437a3
Sujin Ji <120540018+zisuzin@users.noreply.github.com>
2023-08-28 18:36:44 +0900

    backend폴더 npm 모듈 설치

--- backend/address/db.php
+++ backend/address/db.php
@@ -1,13 +1,15 @@
 <?php
-function db_get_pdo()
-{
-    $host = 'localhost';
-    $port = '3306';
-    $dbname = 'shoopen_db';
-    $charset = 'utf8';
-    $username = 'php3min';
-    $db_pw = "1234";
-    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
-    $pdo = new PDO($dsn, $username, $db_pw);
-    return $pdo;
-}
+    $host = "localhost";
+    $user = "root";
+    $pw = "c_MfVWJG/R.ZQT-J";
+    $dbName = "shoopen_db";
+
+    /* MySQL 접속 */
+    $conn = new mysqli($host, $user, $pw, $dbName);
+    
+    /* DB 연결 확인 */
+    if($conn){ echo "접속 성공"."<br>"; }
+    else{ die( '데이터베이스 접속 실패: ' . mysqli_error($conn) ); }
+    
+    mysqli_close($conn);
+?>
