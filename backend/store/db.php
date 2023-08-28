5d60489f44c423c23b210edd53d63ccf1e305911
Sujin Ji <120540018+zisuzin@users.noreply.github.com>
2023-08-28 19:57:44 +0900

    db php 수정

--- backend/store/db.php
+++ backend/store/db.php
@@ -8,8 +8,12 @@
     $conn = new mysqli($host, $user, $pw, $dbName);
     
     /* DB 연결 확인 */
-    if($conn){ echo "접속 성공"."<br>"; }
-    else{ die( '데이터베이스 접속 실패: ' . mysqli_error($conn) ); }
+    if (mysqli_error($conn)) {
+        echo "MySQL 접속 실패!!", "<br>";
+        echo "오류 원인:", mysqli_error();
+        exit();
+    }
+    echo "MySQL 접속 성공!!";
     
     mysqli_close($conn);
 ?>
