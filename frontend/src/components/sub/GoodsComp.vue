30d4629ae70558b3c4dfe4ba8a0f7c5e0ae7354a
지수진 <zi_soo_zin@naver.com>
2023-09-03 14:22:51 +0900

    swiper test1

--- frontend/src/components/sub/GoodsComp.vue
+++ frontend/src/components/sub/GoodsComp.vue
@@ -108,34 +108,30 @@
                             <!-- 이미지 배너 박스 -->
                             <div class="detail_img">
                                 <!-- 슬라이드 이미지 -->
-                                <div class="pSwiper0 mySwiper">
-                                    <ul class="swiper-wrapper">
-                                        <li class="swiper-slide">
-                                            <img :src="'/images/goods/' + this.$route.params.cat1 + '/' +$store.state.dtimg + '.jpg'" alt="썸네일 대표이미지"/>
-                                        </li>
-                                        <li class="swiper-slide">
-                                            <img :src="'/images/goods/' + this.$route.params.cat1 + '/' + this.$route.params.cat2 + '/' + $store.state.dtsumimg2 + '.jpg'" alt="썸네일 상세이미지1"/>
-                                        </li>
-                                        <li class="swiper-slide">
-                                            <img :src="'/images/goods/' + this.$route.params.cat1 + '/' + this.$route.params.cat2 + '/' + $store.state.dtsumimg3 + '.jpg'" alt="썸네일 상세이미지2"/>
-                                        </li>
-                                    </ul>
+                                <swiper :navigation="true" :modules="modules" class="pSwiper0 mySwiper">
+                                    <swiper-slide>
+                                        <img :src="'/images/goods/' + this.$route.params.cat1 + '/' +$store.state.dtimg + '.jpg'" alt="썸네일 대표이미지"/>
+                                    </swiper-slide>
+                                    <swiper-slide>
+                                        <img :src="'/images/goods/' + this.$route.params.cat1 + '/' + this.$route.params.cat2 + '/' + $store.state.dtsumimg2 + '.jpg'" alt="썸네일 상세이미지1"/>
+                                    </swiper-slide>
+                                    <swiper-slide>
+                                        <img :src="'/images/goods/' + this.$route.params.cat1 + '/' + this.$route.params.cat2 + '/' + $store.state.dtsumimg3 + '.jpg'" alt="썸네일 상세이미지2"/>
+                                    </swiper-slide>
                                     <div class="swiper-button-next toRbtn"></div>
                                     <div class="swiper-button-prev toLbtn"></div>
-                                </div>
+                                </swiper>
                                 <!-- 썸네일 리스트 -->
                                 <div class="pSwiper1 mySwiper">
-                                    <ul class="swiper-wrapper">
-                                        <li class="swiper-slide">
-                                            <img :src="'/images/goods/' + this.$route.params.cat1 + '/' + $store.state.dtimg + '.jpg'" alt="썸네일 상세이미지1"/>
-                                        </li>
-                                        <li class="swiper-slide">
-                                            <img :src="'/images/goods/' + this.$route.params.cat1 + '/' + this.$route.params.cat2 + '/' + $store.state.dtsumimg2 + '.jpg'" alt="썸네일 상세이미지2"/>
-                                        </li>
-                                        <li class="swiper-slide">
-                                            <img :src="'/images/goods/' +this.$route.params.cat1 +'/' +this.$route.params.cat2 +'/' +$store.state.dtsumimg3 +'.jpg'" alt="썸네일 상세이미지3"/>
-                                        </li>
-                                    </ul>
+                                    <swiper-slide>
+                                        <img :src="'/images/goods/' + this.$route.params.cat1 + '/' + $store.state.dtimg + '.jpg'" alt="썸네일 상세이미지1"/>
+                                    </swiper-slide>
+                                    <swiper-slide>
+                                        <img :src="'/images/goods/' + this.$route.params.cat1 + '/' + this.$route.params.cat2 + '/' + $store.state.dtsumimg2 + '.jpg'" alt="썸네일 상세이미지2"/>
+                                    </swiper-slide>
+                                    <swiper-slide>
+                                        <img :src="'/images/goods/' +this.$route.params.cat1 +'/' +this.$route.params.cat2 +'/' +$store.state.dtsumimg3 +'.jpg'" alt="썸네일 상세이미지3"/>
+                                    </swiper-slide>
                                 </div>
                             </div>
                             <!-- 구매 인터페이스 영역 -->
@@ -271,13 +267,24 @@ import $ from 'jquery';
 // 디테일페이지 CSS
 import "../../css/detail.css";
 // 스와이퍼 불러오기
-import swiperFn from "../../js/swiper.js";
+// import swiperFn from "../../js/swiper.js";
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 export default {
     name: 'GoodsComp',
     mixins: [crossMixin],
     components: {
         FootComp,
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+      return {
+        modules: [Navigation],
+      };
     },
     data() {
         return {
@@ -322,7 +329,7 @@ export default {
             this.showDt = true;
 
             // 스와이퍼 함수 호출
-            swiperFn();
+            // swiperFn();
         },
         // lnb클릭시 v-if 조건값 설정하는 메서드
         setCatnum(num) {
--- frontend/src/js/swiper.js
+++ frontend/src/js/swiper.js
@@ -1,29 +1,28 @@
-// 스와이퍼 불러오기
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.css';
+// // 스와이퍼 불러오기
+// import { Swiper} from 'swiper/vue';
 
-// 스와이퍼 함수
-function swiperFn() {
-    // 상품 상세페이지 스와이퍼 배너
-    const swiper2 = new Swiper(".pSwiper1", {
-        loop: true,
-        spaceBetween: 10,
-        slidesPerView: 3,
-        freeMode: true,
-        watchSlidesProgress: true,
-    });
+// // 스와이퍼 함수
+// function swiperFn() {
+//     // 상품 상세페이지 스와이퍼 배너
+//     const swiper2 = new Swiper(".pSwiper1", {
+//         loop: true,
+//         spaceBetween: 10,
+//         slidesPerView: 3,
+//         freeMode: true,
+//         watchSlidesProgress: true,
+//     });
     
-    new Swiper(".pSwiper0", { 
-        loop: true,
-        slidesPerView: 1,
-        navigation: {
-            nextEl: ".toRbtn",
-            prevEl: ".toLbtn",
-        },
-        thumbs: {
-            swiper: swiper2,
-        },
-    });
-} ////// swiperFn 함수 ///////
+//     new Swiper(".pSwiper0", { 
+//         loop: true,
+//         slidesPerView: 1,
+//         navigation: {
+//             nextEl: ".toRbtn",
+//             prevEl: ".toLbtn",
+//         },
+//         thumbs: {
+//             swiper: swiper2,
+//         },
+//     });
+// } ////// swiperFn 함수 ///////
 
-export default swiperFn;
+// export default swiperFn;
