4194e61b6996fc34df5529f84cf8c165f95b7015
지수진 <zi_soo_zin@naver.com>
2023-09-13 21:58:49 +0900

    [추가] SideComp a태그 기본기능 막는 코드 추가

--- frontend/src/components/common/SideComp.vue
+++ frontend/src/components/common/SideComp.vue
@@ -1,9 +1,9 @@
 <template>
     <ul class="side">
         <!-- 3. 사이드영역 -->
-        <li><a href="#" class="ico_mypage" @click="loginFn()"></a></li>
-        <li><a href="#" class="ico_cart"></a></li>
-        <li class="wishbtn" @click="wishFn()">
+        <li><a href="#" class="ico_mypage" @click.prevent="loginFn()"></a></li>
+        <li><a href="#" class="ico_cart" @click.prevent></a></li>
+        <li class="wishbtn" @click.prevent="wishFn()">
             <a href="#" class="ico_wish"></a>
             <div class="callout">{{$store.state.callout}}</div>
         </li>
@@ -17,13 +17,13 @@ import $ from "jquery";
 export default {
     name: 'SideComp',
     methods: {
-    wishFn() {
-      $(".wish_comp").toggleClass("show").siblings().removeClass("show");
+        wishFn() {
+            $(".wish_comp").toggleClass("show").siblings().removeClass("show");
+        },
+        loginFn() {
+            $(".user_comp").toggleClass("show").siblings().removeClass("show");
+        },
     },
-    loginFn() {
-      $(".user_comp").toggleClass("show").siblings().removeClass("show");
-    },
-  },
 }
 </script>
 
