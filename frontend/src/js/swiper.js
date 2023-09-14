88d9bdc2ab6019db66e3ca78f43fcc65fd8344ee
지수진 <zi_soo_zin@naver.com>
2023-09-14 18:57:49 +0900

    [test] 디테일페이지 액티브 스와이퍼에 따라서 썸네일스와이퍼 조정

--- frontend/src/js/swiper.js
+++ frontend/src/js/swiper.js
@@ -1,31 +1,25 @@
 // 스와이퍼 불러오기
 import Swiper from 'swiper/bundle';
 import 'swiper/swiper-bundle.css';
-// 제이쿼리 불러오기
-import $ from "jquery";
 
 // 스와이퍼 함수
 function swiperFn() {
-    const swiper2 = new Swiper(".mySwiper2", {
+    const swiper2 = new Swiper('.mySwiper2', {
         spaceBetween: 10,
         slidesPerView: 3,
         resistanceRatio: 0, // 드래그시 저항 막기
     });
-    
-    new Swiper(".mySwiper1", { 
+
+    new Swiper('.mySwiper1', {
         navigation: {
-            nextEl: ".toRbtn",
-            prevEl: ".toLbtn",
+            nextEl: '.toRbtn',
+            prevEl: '.toLbtn',
         },
         thumbs: {
             swiper: swiper2,
         },
         resistanceRatio: 0, // 드래그시 저항 막기
     });
-
-    const tg = $(".mySwiper1 .swiper-slide-active").find("img").attr("src")
-    console.log(tg)
-
 } ////// swiperFn 함수 ///////
 
 export default swiperFn;
