5d8b07d11902315b74f098ddc13446c97c552ccb
지수진 <zi_soo_zin@naver.com>
2023-09-14 17:17:34 +0900

    [추가] 디테일페이지 스와이퍼 드래그 저항막는 코드 추가

--- frontend/src/js/swiper.js
+++ frontend/src/js/swiper.js
@@ -1,18 +1,18 @@
 // 스와이퍼 불러오기
 import Swiper from 'swiper/bundle';
 import 'swiper/swiper-bundle.css';
+// 제이쿼리 불러오기
+import $ from "jquery";
 
 // 스와이퍼 함수
 function swiperFn() {
     const swiper2 = new Swiper(".mySwiper2", {
         spaceBetween: 10,
         slidesPerView: 3,
-        freeMode: true,
-        watchSlidesProgress: true,
+        resistanceRatio: 0, // 드래그시 저항 막기
     });
     
     new Swiper(".mySwiper1", { 
-        loop: true,
         navigation: {
             nextEl: ".toRbtn",
             prevEl: ".toLbtn",
@@ -20,7 +20,12 @@ function swiperFn() {
         thumbs: {
             swiper: swiper2,
         },
+        resistanceRatio: 0, // 드래그시 저항 막기
     });
+
+    const tg = $(".mySwiper1 .swiper-slide-active").find("img").attr("src")
+    console.log(tg)
+
 } ////// swiperFn 함수 ///////
 
 export default swiperFn;
