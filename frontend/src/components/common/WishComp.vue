36993fc40c4ae98b1b447760a027900fe8f872a7
지수진 <zi_soo_zin@naver.com>
2023-09-14 19:41:42 +0900

    [추가] 위시리스트 수량 증감(+/-) 기능 추가

--- frontend/src/components/common/WishComp.vue
+++ frontend/src/components/common/WishComp.vue
@@ -3,10 +3,37 @@
 import crossMixin from '@/js/common.js';
 // 공통 템플릿 데이터
 import comData from '@/js/tempData/comComp.js';
+// 제이쿼리 불러오기
+import $ from 'jquery';
 
 export default {
     name: 'WishComp',
     template: comData.wishComp,
     mixins: [crossMixin],
+    methods: {
+        // 더하기 함수
+        minusBtn() {
+            let tg = event.currentTarget;
+            let num = $(tg).siblings().eq(1).find('input').val();
+
+            num--;
+
+            // -값 방지
+            if (num == 0) return;
+
+            // 수량 업데이트
+            $(tg).siblings().eq(1).find('input').val(num);
+        },
+        // 빼기 함수
+        plusBtn() {
+            let tg = event.currentTarget;
+            let num = $(tg).siblings().eq(1).find('input').val();
+
+            num++;
+
+            // 수량 업데이트
+            $(tg).siblings().eq(1).find('input').val(num);
+        },
+    },
 };
 </script>
