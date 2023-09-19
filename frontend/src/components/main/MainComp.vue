2663f5406090b029b6a0293498784ffc6c3d2d1e
지수진 <zi_soo_zin@naver.com>
2023-09-19 23:18:41 +0900

    jenkins test5

--- frontend/src/components/main/MainComp.vue
+++ frontend/src/components/main/MainComp.vue
@@ -357,7 +357,7 @@ export default {
         }
 
         window.addEventListener("scroll", moveSec);
-        console.log("jenkins 테스트중4444!! 제발!2")
+        console.log("jenkins 테스트중5! 과연?")
 
         // 쵤초호출!
         bannerSwiper();
