622599594911e30ea20b3e08fcca3e8b48f40e3a
지수진 <zi_soo_zin@naver.com>
2023-10-21 00:39:40 +0900

    [변경] eslintrc.js 파일 코드 제거

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
     root: true,
     env: {
         node: true,
-        "jest": true
     },
     extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
     parserOptions: {
