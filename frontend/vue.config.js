627c4da082f0aba35ad62e18f51aaf918dc4bf54
지수진 <zi_soo_zin@naver.com>
2023-11-06 00:43:15 +0900

    [추가] firebase deploy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/frontend/dist/' : '/',
+    // publicPath: process.env.NODE_ENV === 'production' ? '/frontend/dist/' : '/',
     devServer: {
         proxy: {
             '/api': {
